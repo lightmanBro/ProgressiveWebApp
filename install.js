fe68a6d107a1892a74f028192c68658745ef3ab2
LightmanBro <omotosodavidromeo@gmail.com>
2023-06-19 09:24:07 +0100

    ...

--- install.js
+++ install.js
@@ -3,29 +3,28 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   deferredPrompt = event;
-
-  // Show the install button
-  const installButton = document.getElementById('install-button');
-  installButton.style.display = 'block';
-  installButton.addEventListener('click', installPWA);
+});
+// Install the PWA when the install button is clicked
+document.getElementById('install-button').addEventListener('click',()=>{
+    installPWA();
+    console.log('working')
 });
 
+
 function installPWA() {
-  // Prompt the user to install the PWA
-  deferredPrompt.prompt();
-
-  // Wait for the user's choice
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the PWA installation');
-    } else {
-      console.log('User dismissed the PWA installation');
-    }
-
-    deferredPrompt = null;
-
-    // Hide the install button
-    const installButton = document.getElementById('install-button');
-    installButton.style.display = 'none';
-  });
+  if (deferredPrompt) {
+    // Prompt the user to install the PWA
+    deferredPrompt.prompt();
+    
+    // Wait for the user's choice
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the PWA installation');
+      } else {
+        console.log('User dismissed the PWA installation');
+      }
+      
+      deferredPrompt = null;
+    });
+  }
 }
